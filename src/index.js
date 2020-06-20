1dd5e485e349dca1fc4ad46fd2529602b2859f49
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2020-06-20 01:01:33 -0300

    SignIn route

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require("./controllers/authController.js")(app);
+
+app.get("/", (req, res) => {
+	res.send({ message: "Hello Auth" });
+});
+
+app.listen(3000);
